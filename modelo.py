feb316b1680aed4c4262e27069415832dff5fe87
greatooau <pajaroazulx@gmail.com>
2022-05-13 23:38:33 -0500

    ben?

--- modelo.py
+++ modelo.py
@@ -1,45 +1,7 @@
 import random
 
-def simular(TLimite, numTaqAbiertas, premiere):
-    esEstreno = False
-    while True:
-        try:
-            TLimite = int(input("Ingrese el tiempo Limite de la simulación en minutos: "))
-        except:
-            print("\n\nIngrese un número entero.\n")
-
-        if  TLimite > 0:
-            break
-        else: 
-            print("\nIngrese un número mayor a 0\n.")
-
-    while True:
-        try:
-            numTaqAbiertas = int(input("\nIngrese el número de taquillas que se abrirán: "))
-        except:
-            print("\n\nIngrese un número entero.\n")
-
-        if  numTaqAbiertas > 0 and numTaqAbiertas <= 5:
-            break
-        else: 
-            print("\n\nIngrese un número que sea mayor a 0 y menor a 6.\n")
-
-    while True:
-        _input = input("¿La simulación será de un estreno? [ Si / No ]\n")
-
-        flag = False
-
-        for letter in _input:
-            if letter.isdigit():
-                flag = True
-
-        if  _input.lower() == "si" and not flag:
-            esEstreno = True
-            break
-        elif _input.lower() == "no" and not flag:
-            break
-        else: 
-            print("\n\nIngrese una de las opciones.\n")
+def simular(TLimite:int, numTaqAbiertas:int, premiere:int):
+    esEstreno = True if premiere == 1 else False
 
     def generarVariableAleatoria(media, desv):
         sumR = 0
@@ -52,7 +14,7 @@ def simular(TLimite, numTaqAbiertas, premiere):
     tiempoAtencion = []
     tiempoPromedio = []
     taquillaLibre = []
-    tiempoCaja = []
+    tiempoTaquilla = []
     personasAtendidas = []
 
     if esEstreno:
@@ -66,7 +28,7 @@ def simular(TLimite, numTaqAbiertas, premiere):
     for i in range(numTaqAbiertas):
         tiempoAtencion.append(generarVariableAleatoria(media, desv))
         taquillaLibre.append(False)
-        tiempoCaja.append(0)
+        tiempoTaquilla.append(0)
         personasAtendidas.append(0)
 
     tiempoSimulacion = 0
@@ -85,13 +47,13 @@ def simular(TLimite, numTaqAbiertas, premiere):
                 personasAtendidas[i] += 1
                 tiempoSimulacion += delta
                 taquillaLibre[i] = True
-                tiempoCaja[i] += delta
+                tiempoTaquilla[i] += delta
 
     totalPersonasAtendidas = 0
     for i in range(numTaqAbiertas):
 
         totalPersonasAtendidas += personasAtendidas[i]
-        tiempoPromedio.append(tiempoCaja[i] / personasAtendidas[i])
+        tiempoPromedio.append(tiempoTaquilla[i] / personasAtendidas[i])
         print(f"Tiempo promedio de la caja {i + 1}: {tiempoPromedio[i]}")
         print(f"Personas atendidas en la caja {i + 1}: {personasAtendidas[i]}")
         print("\n")
@@ -100,5 +62,11 @@ def simular(TLimite, numTaqAbiertas, premiere):
 
     infoToSubmit = []
     [infoToSubmit.append([personasAtendidas[x], tiempoPromedio[x]]) for x in range(numTaqAbiertas)]
-
-    return "taquillas (1-5), premiere (1-2), tiempoLimite(m), totalPersonas (int), infoToSubmit"
+        #"taquillas (1-5), premiere (1-2), tiempoLimite(m), totalPersonas (int), infoToSubmit"
+    return {
+        "taquillas_abiertas":numTaqAbiertas,
+        "premiere":premiere,
+        "tiempo_limite":TLimite,
+        "total_personas":totalPersonasAtendidas,
+        "infoToSubmit":infoToSubmit
+    }
